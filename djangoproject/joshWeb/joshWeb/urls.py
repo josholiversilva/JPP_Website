c5865e5cfc3d405eeb2d6c972d3a679319d0f838
Josh Silva <joshsilva@Joshs-MBP.home>
2020-12-24 16:25:35 -0800

    Fix centering & sizing

--- djangoproject/joshWeb/joshWeb/urls.py
+++ djangoproject/joshWeb/joshWeb/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('main.urls')),
+    path('contact/', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
--- djangoproject/joshWeb/main/views.py
+++ djangoproject/joshWeb/main/views.py
@@ -4,11 +4,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+loader.get_template('main/base.html')
 
 def index(request):
     template = loader.get_template('main/index.html')
     context = {
         'latest_question_list': 'latest_question_list',
     }
-    loader.get_template('main/base.html')
+    return HttpResponse(template.render(context,request))
+
+def contact(request):
+    template = loader.get_template('main/contact.html')
+    context = {
+        'latest_question_list': 'latest_question_list',
+    }
     return HttpResponse(template.render(context,request))
